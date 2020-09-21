ceb5999af8e60d37162965a2c1e12ca64d66ced1
angelbojorquez <69012992+angelbojorquez@users.noreply.github.com>
2020-09-20 21:15:51 -0600

    SegundoParcial

---
+++ src/sockets.js
@@ -0,0 +1,19 @@
+module.exports = io => {
+
+    // keep track of all lines that client sends
+    var line_history = [];
+  
+    io.on('connection', socket => {
+      console.log('Nuevo Usuario Conectado')
+      
+      for (let i in line_history) {
+        socket.emit('draw_line', {line: line_history[i]});
+      }
+  
+      socket.on('draw_line', data => {
+        line_history.push(data.line);
+        io.emit('draw_line', { line: data.line });
+      });
+    });
+  
+  };
